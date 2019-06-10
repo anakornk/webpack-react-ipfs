2c4efadf364b6f7b5ab9954eaebec005da9b41b9
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-06-10 17:22:05 +0800

    added ipfs support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IPFS from 'ipfs';
 
-const title = 'My Minimal React Webpack Babel Setup';
+const node = new IPFS()
+
+node.on('ready', async () => {
+  try {
+    const version = await node.version()
+    console.log('Version:', version.version)
+  } catch (error) {
+    console.error('Error', error)
+  }
+})
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <div>Hello World</div>,
   document.getElementById('app')
 );
 
